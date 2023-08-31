635d0532e00cd14d3c4e1c90b20f5f51d5202a00
Peter Kofler <peter.kofler@code-cop.org>
2023-08-31 19:50:56 +0200

    Sample code for a Rest client, needed by rooms.

---
+++ template-java-micronaut/src/test/java/org/codecop/rogue/template/client/JsonRestClientTest.java
@@ -0,0 +1,68 @@
+package org.codecop.rogue.template.client;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.codecop.rogue.template.client.JsonRestClient;
+import org.codecop.rogue.template.client.JsonText;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+
+@MicronautTest(environments = { "wiremock" })
+class JsonRestClientTest {
+
+    @Inject
+    WireMockServer wireMockServer; // at port 9090
+
+    @Inject
+    JsonRestClient client;
+
+    @Test
+    void testHelloJsonResponse() {
+        wireMockServer.stubFor(get(urlEqualTo("/bar")) //
+                .willReturn(aResponse() //
+                        .withHeader("Content-Type", "application/json") //
+                        .withBody("{\"text\":\"Hello Json\"}")));
+
+        JsonText[] actual = { null };
+        client.fetchReleases().subscribe(new Subscriber<JsonText>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                // System.out.println("onSubscribe " + s);
+                s.request(1);
+            }
+
+            @Override
+            public void onNext(JsonText t) {
+                // System.out.println("onNext " + t);
+                actual[0] = t;
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // System.out.println("onError " + t);
+            }
+
+            @Override
+            public void onComplete() {
+                // System.out.println("onComplete");
+            }
+        });
+
+        await().atMost(1, TimeUnit.SECONDS).until(() -> actual[0] != null);
+
+        assertEquals("Hello Json", actual[0].getText());
+    }
+
+}
