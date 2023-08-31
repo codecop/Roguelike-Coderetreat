1377ef1ccc8533757beb965006f4993d7f66302f
Peter Kofler <peter.kofler@code-cop.org>
2023-08-31 20:47:39 +0200

    Massive refactoring: split coordinates, extract methods, names.

--- room-sample/src/main/java/org/codecop/rogue/room1/Room.java
+++ room-sample/src/main/java/org/codecop/rogue/room1/Room.java
@@ -2,8 +2,7 @@ package org.codecop.rogue.room1;
 
 public class Room {
 
-    private int playerPos = 1 * (7 + 1) + 3;
-    private char[] initial = ("" + //
+    private final char[] initialLayout = ("" + //
             "#######\n" + //
             "#     #\n" + //
             "#     #\n" + //
@@ -11,39 +10,72 @@ public class Room {
             "#     #\n" + //
             "#     #\n" + //
             "#######\n").toCharArray();
+    private final int columns = new String(initialLayout).replaceAll("\n.*", "").length();
+    // private final int rows = new String(initialLayout).replaceAll("[^\n]", "").length();
+
+    private int playerX = 3;
+    private int playerY = 1;
 
     public String display() {
-        char[] x = initial.clone();
-        setPlayer(x);
-        return new String(x);
+        char[] layout = initialLayout.clone();
+        setPlayerTo(layout);
+        return new String(layout);
     }
 
-    private void setPlayer(char[] x) {
-        x[playerPos] = '@';
+    private void setPlayerTo(char[] layout) {
+        layout[playerAsIndex()] = '@';
+    }
+
+    private int playerAsIndex() {
+        return asIndex(playerX, playerY);
     }
 
     public void playerMoves(char direction) {
         switch (direction) {
-        case 'd':
-            if (free(playerPos + 1))
-                playerPos += 1;
-            break;
-        case 'a':
-            if (free(playerPos - 1))
-                playerPos -= 1;
-            break;
         case 'w':
-            if (free(playerPos - (7 + 1)))
-                playerPos -= (7 + 1);
+            playerMoveUp();
+            break;
+        case 'd':
+            playerMoveRight();
             break;
         case 's':
-            if (free(playerPos + (7 + 1)))
-                playerPos += (7 + 1);
+            playerMoveDown();
+            break;
+        case 'a':
+            playerMoveLeft();
             break;
         }
     }
 
-    private boolean free(int pos) {
-        return initial[pos] == ' ';
+    private void playerMoveUp() {
+        if (layoutIsEmptyAt(playerX, playerY - 1)) {
+            playerY -= 1;
+        }
+    }
+
+    private void playerMoveRight() {
+        if (layoutIsEmptyAt(playerX + 1, playerY)) {
+            playerX += 1;
+        }
+    }
+
+    private void playerMoveDown() {
+        if (layoutIsEmptyAt(playerX, playerY + 1)) {
+            playerY += 1;
+        }
+    }
+
+    private void playerMoveLeft() {
+        if (layoutIsEmptyAt(playerX - 1, playerY)) {
+            playerX -= 1;
+        }
+    }
+
+    private boolean layoutIsEmptyAt(int x, int y) {
+        return initialLayout[asIndex(x, y)] == ' ';
+    }
+
+    private int asIndex(int x, int y) {
+        return y * (columns + 1) + x;
     }
 }
