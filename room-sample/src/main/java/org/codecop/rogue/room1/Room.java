53b7c93ef916a6694f70650d3595730b67b6affa
Peter Kofler <peter.kofler@code-cop.org>
2023-08-31 20:21:18 +0200

    1st test: room displays fake.

---
+++ room-sample/src/main/java/org/codecop/rogue/room1/Room.java
@@ -0,0 +1,16 @@
+package org.codecop.rogue.room1;
+
+public class Room {
+
+    public String display() {
+        return "" + //
+                "#######\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#######\n";
+    }
+
+}
---
+++ room-sample/src/test/java/org/codecop/rogue/room1/RoomTest.java
@@ -0,0 +1,25 @@
+package org.codecop.rogue.room1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RoomTest {
+
+    @Test
+    void roomHasWalls() {
+        Room room = new Room();
+
+        String display = room.display();
+
+        assertEquals("" + //
+                "#######\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#     #\n" + //
+                "#######\n", display);
+    }
+
+}
