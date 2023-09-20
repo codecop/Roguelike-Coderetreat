dceceb22e122ff04012457255dd72ef27c8833d3
g.aichinger <gabriel.aichinger@gmail.com>
2023-09-20 12:21:48 +0200

    Task 3 WIP

--- nullpointer/src/app.ts
+++ nullpointer/src/app.ts
@@ -1,12 +1,33 @@
 import express from "express";
 import Hello from "./Hello";
-
+import Game from './Game';
 const hello = new Hello();
 
 async function createApp() {
     const app = express();
     app.use(express.json());
+    const game = new Game();
 
+    app.get("/nullpointer", async (_req, res) => {
+
+        res.json({ "layout": game.getCurrentRoom()});
+
+    });
+/*
+    app.post("/nullpointer", async (req, res) => {
+        const { row, column } = req.body;
+        game.player.setPlayerPosition(row, column);
+
+
+        if (name != undefined) {
+            hello.setName(name);
+            res.status(201).json();
+        } else {
+            res.status(400).json();
+        }
+
+    });
+    */
     app.get("/hello", async (_req, res) => {
 
         res.json({ "name": hello.getName() });
