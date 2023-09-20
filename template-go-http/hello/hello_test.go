3f39fc3513653550c5e5403dba3da67d6cb3133a
Paul Opuchlich <p.opuchlich@sportradar.com>
2023-09-20 07:55:40 +0200

    go template with http client created

---
+++ template-go-http/hello/hello_test.go
@@ -0,0 +1,27 @@
+package hello_test
+
+import (
+	"hello"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetName(t *testing.T) {
+	name := "World!"
+	hello := hello.NewHello(name)
+
+	assert.Equal(t, hello.GetName(), name)
+
+}
+
+func TestSetName(t *testing.T) {
+	name := "World!"
+	newName := "Paul"
+	hello := hello.NewHello(name)
+
+	hello.SetName(newName)
+
+	assert.Equal(t, hello.GetName(), newName)
+
+}
