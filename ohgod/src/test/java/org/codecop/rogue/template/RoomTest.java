90c20b2650f25b8a3ea2bfab0c9a6bd837cd5b88
Sebastian Lukas <s.lukas@sportradar.com>
2023-09-20 12:22:53 +0200

    first commit. in the middle of task3

---
+++ ohgod/src/test/java/org/codecop/rogue/template/RoomTest.java
@@ -0,0 +1,87 @@
+package org.codecop.rogue.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RoomTest {
+
+    @Test
+    void shouldPrint() {
+        Room room = new Room();
+
+        String map = room.toString();
+
+        assertEquals(
+                        "#######\n" +
+                        "#@    #\n" +
+                        "|     #\n" +
+                        "#     #\n" +
+                        "#######\n",
+                map);
+    }
+    @Test
+    void shouldHaveDoor(){
+        Room room = new Room();
+
+        String map = room.toString();
+
+        boolean hasDoor = map.indexOf('|') > 0;
+        assertTrue(hasDoor );
+    }
+
+    @Test
+    void shouldChangePosition(){
+        Room room = new Room();
+        String t0 = room.toString();
+        room.setNewPosition(1,2);
+        String t1 = room.toString();
+
+        assertEquals(
+                        "#######\n" +
+                        "#@    #\n" +
+                        "|     #\n" +
+                        "#     #\n" +
+                        "#######\n",
+                t0);
+
+        assertEquals(
+                        "#######\n" +
+                        "# @   #\n" +
+                        "|     #\n" +
+                        "#     #\n" +
+                        "#######\n",
+                t1);
+
+
+    }
+
+    @Test
+    void shouldMoveRightTwice(){
+        Room room = new Room();
+        String t0 = room.toString();
+        room.setNewPosition(1,3);
+        String t1 = room.toString();
+
+        assertEquals(
+                        "#######\n" +
+                        "#@    #\n" +
+                        "|     #\n" +
+                        "#     #\n" +
+                        "#######\n",
+                t0);
+
+        assertEquals(
+                        "#######\n" +
+                        "#  @  #\n" +
+                        "|     #\n" +
+                        "#     #\n" +
+                        "#######\n",
+                t1);
+
+
+    }
+
+
+}
