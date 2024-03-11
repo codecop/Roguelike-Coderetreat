83b9ac389d600333e41711432cd1dfb69d73b62f
Peter Kofler <peter.kofler@code-cop.org>
2024-03-11 20:42:51 +0100

    Maven project with empty Main class.

---
+++ room-tester/src/main/java/org/codecop/rogue/tester/Main.java
@@ -0,0 +1,12 @@
+package org.codecop.rogue.tester;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Testing room " + args[0]);
+//        System.out.println("Inventory started on " + PORT + ",\n" + //
+//                "Open http://localhost:" + PORT + "/inventory");
+
+    }
+
+}
