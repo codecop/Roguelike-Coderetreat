3e616db8662c621c5a9f20371b26b84a3787c4ea
j.fernandez <j.fernandezsalcedo@sportradar.com>
2025-10-15 11:22:48 +0200

    before extracting build_map

--- jj/main.go
+++ jj/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"roguelike-go/hello"
-	"net/http"
+	"roguelike-go/room"
 )
 
 func main() {
-	http.HandleFunc("/get_name", hello.GetName)
-	http.HandleFunc("/set_name", hello.SetName)
+	// http.HandleFunc("/get_name", hello.GetName)
+	// http.HandleFunc("/set_name", hello.SetName)
 
-	fmt.Println("Server is listening on :8080")
-	http.ListenAndServe(":8080", nil)
+	// fmt.Println("Server is listening on :8080")
+	// http.ListenAndServe(":8080", nil)
+
+	r := room.NewRoom(8, 6, []room.Coordinate{{X: 5, Y: 5}})
+	player := room.NewPlayer(room.Coordinate{X: 2, Y: 3}, '@')
+	r.AddPlayer(&player)
+	room.Print(r)
+
+	r.SetNewPosition(3, 4)
+	room.Print(r)
+
+	// fmt.Println(hello.Hello{name: "JJ"}.GetName())
+	// fmt.Println(hello.Hello{name: "JJ"}.SetName("Juanjo"))
+	// fmt.Println(hello.Hello{name: "JJ"}.GetName())
 }
