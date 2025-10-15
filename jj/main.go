704a0dab96314bccee1104b2d24a5b8673164e86
j.fernandez <j.fernandezsalcedo@sportradar.com>
2025-10-15 16:27:21 +0200

    fix

--- jj/main.go
+++ jj/main.go
@@ -10,11 +10,11 @@ import (
 
 func main() {
 
-	jjRoom := room.NewRoom("jj-room", "Our first room", 20, 20, room.Coordinate{X: 19, Y: 19})
+	jjRoom := room.NewRoom("jj-room", "Our first room", 15, 15, room.Coordinate{X: 14, Y: 14})
 	player := room.NewPlayer(room.Coordinate{X: 1, Y: 1}, '@')
 	jjRoom.AddPlayer(&player)
 
-	key := room.NewKey(room.Coordinate{X: 15, Y: 15}, 'K')
+	key := room.NewKey(room.Coordinate{X: 12, Y: 12}, 'c')
 	jjRoom.AddKey(&key)
 
 	monster := room.NewMonster(room.Coordinate{X: 10, Y: 10}, 'M')
